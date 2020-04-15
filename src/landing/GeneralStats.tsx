90c89ffa05fe444d143463d135cb7c4def74d80b
Massimiliano Ziccardi <mziccard@redhat.com>
2020-04-15 16:51:51 +0200

    Added application list page

--- src/landing/GeneralStats.tsx
+++ src/landing/GeneralStats.tsx
@@ -1,6 +1,6 @@
-import {PushApplication} from '@aerogear/unifiedpush-admin-client';
-import React, {Component} from 'react';
-import {Divider, List, ListItem, ListVariant, Text, TextVariants, Title,} from '@patternfly/react-core';
+import { PushApplication } from '@aerogear/unifiedpush-admin-client';
+import React, { Component } from 'react';
+import { Divider, List, ListItem, ListVariant, Text, TextVariants, Title, } from '@patternfly/react-core';
 
 interface Props {
   apps: PushApplication[];
@@ -18,10 +18,10 @@ export class GeneralStats extends Component<Props> {
           General Statistics
         </Title>
         <Divider />
-        <List variant={ListVariant.inline} style={{marginLeft: 20, marginTop: 20}}>
-          <ListItem><div><Text className={'ups-count'}>2</Text><Text style={{fontSize: 15, color: '#999'}}>Apps</Text></div></ListItem>
-          <ListItem><div><Text style={{fontSize: 30, color: '#999'}}>0</Text><Text style={{fontSize: 15, color: '#999'}}>Messages</Text></div></ListItem>
-          <ListItem><div><Text style={{fontSize: 30, color: '#999'}}>0</Text><Text style={{fontSize: 15, color: '#999'}}>Devices</Text></div></ListItem>
+        <List variant={ListVariant.inline} style={{ marginLeft: 20, marginTop: 20 }}>
+          <ListItem><div><Text className={'ups-count'}>2</Text><Text style={{ fontSize: 15, color: '#999' }}>Apps</Text></div></ListItem>
+          <ListItem><div><Text style={{ fontSize: 30, color: '#999' }}>0</Text><Text style={{ fontSize: 15, color: '#999' }}>Messages</Text></div></ListItem>
+          <ListItem><div><Text style={{ fontSize: 30, color: '#999' }}>0</Text><Text style={{ fontSize: 15, color: '#999' }}>Devices</Text></div></ListItem>
         </List>
       </div>
     );
